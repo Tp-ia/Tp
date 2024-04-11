fd0470a9a2640b11a22ab9514b8607b02fbfe3ab
Corentin <corentin.ludwig@outlook.fr>
2024-04-11 17:33:00 +0200

    parcour

---
+++ parcour/profondeur_dabord.cpp
@@ -0,0 +1,17 @@
+#include "../jeu/plateau.cpp"
+
+typedef struct{
+    bool but;
+    Etat *e;
+
+} retour ;
+
+bool testEtatBut(Etat *e){}
+
+retour ProfondeurDAbord(Etat *e){
+
+}
+
+int main(){
+    Etat *e = new Etat();
+}
