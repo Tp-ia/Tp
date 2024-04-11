b43a550ac42dd1d6a3f7f5aefdc320b0e0ee9cab
Corentin <corentin.ludwig@outlook.fr>
2024-04-11 08:50:24 +0200

    plateau ajoue add/suppr/...

--- plateau.cpp
+++ plateau.cpp
@@ -1,28 +1,77 @@
 #include <iostream>
 
 
-typedef struct{
-    int nb_cube;
-    int cube[3];
-} tige;
+
 
 class Etat {
     public:
-    tige tiges[3];
-    void move(int tige_depart, int tige_destination);
+        int tiges[4][3];
+        int nb_cube[4];
+        Etat();
+        void print();
+        void add_cube(int tige, int cube);
+        int suppr_cube(int tige);
+        void move(int tige_depart, int tige_destination);
+
 };
 
-void Etat::move(int tige_depart, int tige_destination){
-
-    if(tiges[tige_depart].nb_cube <= 0){
-
-
+Etat::Etat(){
+    for(int i = 0; i<4;i++){
+        for(int j = 0; j<3;j++){
+            tiges[i][j]=0;
+        }
+        nb_cube[i] = 0;
     }
+}
 
-    int cube = tiges[tige_depart].cube[tiges[tige_depart].nb_cube];
+void Etat::print(){
+    for(int i = 2; i>=0;i--){
+        for(int j = 0; j<4;j++){
+            std::cout<<tiges[j][i];
+            std::cout<< " ";
+        }
+        std::cout<< "\n";
+    }
+}
 
+void Etat::add_cube(int tige, int cube){
+    if(nb_cube[tige] >= 3){
+        std::cout<< "tiges deja pleine\n";
+        exit(0);
+    }
+    tiges[tige][nb_cube[tige]] = cube;
+    nb_cube[tige]++;
+}
+
+int Etat::suppr_cube(int tige){
+    if(nb_cube[tige] <= 0){
+        std::cout<< "pas de cube a deplacer\n";
+        exit(0);
+    }
+    nb_cube[tige]--;
+    int cube = tiges[tige][nb_cube[tige]];
+    tiges[tige][nb_cube[tige]] = 0;
+    
+    return cube;
+}
+
+void Etat::move(int tige_depart, int tige_destination){
+    add_cube(tige_destination, suppr_cube(tige_depart));
 }
 
 int main(){
+    Etat *e = new Etat();
+    
+    e->print();
+    std::cout<<"\n";
+    e->add_cube(1, 3);
+    e->print();
+    std::cout<<"\n";
+
+    // e->suppr_cube(1);
+    // e->print();
+    // std::cout<<"\n";
+    e->move(1, 2);
+    e->print();
 
 }
