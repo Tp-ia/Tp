69f576723d7547f8efef86bc7843697b84b934ae
sayuuyu <sayuririn@gmail.com>
2024-04-23 18:32:29 +0200

    profondeur dabord

--- parcour/profondeur_dabord.cpp
+++ parcour/profondeur_dabord.cpp
@@ -17,6 +17,7 @@ std::queue<Etat> filsEtat(Etat *e){
             if(num_tige_arrive!=num_tige_depart && e->tiges[num_tige_depart].size()!=0 && e->tiges[num_tige_arrive].size()<3 ){
                 Etat *fils=e->clone();
                 fils->move(num_tige_depart,num_tige_arrive);
+                fils->level++;
                 filsEtat.push(*fils);
             }
         }
@@ -24,8 +25,48 @@ std::queue<Etat> filsEtat(Etat *e){
     return filsEtat;
 }
 
-retour ProfondeurDAbord(Etat *e){
+bool elementDansQueue(const queue<Etat>& maQueue, Etat* elementRecherche) {
+    queue<Etat> maQueueTemp = maQueue; // Copie de la file pour éviter de la modifier
 
+    while (!maQueueTemp.empty()) {
+        if (maQueueTemp.front().equals(elementRecherche)) {
+            return true; // L'élément est trouvé
+        }
+        maQueueTemp.pop(); // Passe à l'élément suivant
+    }
+
+    return false; // L'élément n'est pas trouvé
+}
+retour ProfondeurDAbord(Etat *e,Etat *etat_but){
+    retour resultat;
+    resultat.but=false;
+    resultat.e=e;
+    stack<Etat> enAttente;
+    queue<Etat> vus;
+    enAttente.push(*e);
+    Etat fils=Etat(0);
+    Etat prochain=Etat(0);
+    queue<Etat> q;
+    while(!enAttente.empty() && !resultat.but){
+        prochain=enAttente.top();
+        enAttente.pop();
+        vus.push(prochain);
+        if(prochain.equals(etat_but)){
+            resultat.but=true;
+            resultat.e=etat_but;
+        }
+        else{
+            q = filsEtat(e);
+            while(!q.empty()){
+                fils = q.front();
+                q.pop();
+                if(!elementDansQueue(vus,&fils)){
+                    enAttente.push(fils);
+                }
+            }
+        }
+    }
+    return resultat;
 }
 
 void printQueue(const queue<Etat>& q) {
