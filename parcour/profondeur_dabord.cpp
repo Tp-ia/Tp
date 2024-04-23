34d5f268735427da2c0717811a668c0adc6ec798
sayuuyu <sayuririn@gmail.com>
2024-04-23 17:41:07 +0200

    condition filsEtat

--- parcour/profondeur_dabord.cpp
+++ parcour/profondeur_dabord.cpp
@@ -11,7 +11,7 @@ std::queue<Etat> filsEtat(Etat *e){
     std::queue<Etat> filsEtat;
     for(int num_tige_depart =0;num_tige_depart<4;num_tige_depart++){
         for(int num_tige_arrive=0; num_tige_arrive<4;num_tige_arrive++){
-            if(num_tige_arrive!=num_tige_depart){
+            if(num_tige_arrive!=num_tige_depart && e->tiges[num_tige_depart].size()==0 && e->tiges[num_tige_arrive].size()>=3 ){
                 Etat *fils=e->clone();
                 fils->move(num_tige_depart,num_tige_arrive);
                 filsEtat.push(*fils);
