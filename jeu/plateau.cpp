e9fbf2da9c495e9b0d2b1341fdd75aaa7c970ff1
Corentin <corentin.ludwig@outlook.fr>
2024-04-23 17:41:56 +0200

    clone filsEtat print

--- jeu/plateau.cpp
+++ jeu/plateau.cpp
@@ -1,13 +1,14 @@
 #include <iostream>
 #include <stack>
 
+using namespace std;
 
 class Etat {
     public:
         std::stack<int> tiges[4];
         int level;
         Etat(int level);
-        // void print();
+        void print();
         void add_cube(int num_tige, int cube);
         int suppr_cube(int num_tige);
         void move(int num_tige_depart, int num_tige_destination);
@@ -16,49 +17,52 @@ class Etat {
 
 };
 
-Etat::Etat(int level){
-    this->level = level;
-}
+Etat::Etat(int level) { this->level = level; }
 
-bool Etat::equals(Etat *e){
-    for(int i = 0; i<4;i++){
-        if(tiges[i]!=e->tiges[i]){
-            return false;
-        }
+bool Etat::equals(Etat *e) {
+  for (int i = 0; i < 4; i++) {
+    if (tiges[i] != e->tiges[i]) {
+      return false;
     }
-    return true;
+  }
+  return true;
 }
 
-// void Etat::print(){
-//     for(int i = 2; i>=0;i--){
-//         for(int j = 0; j<4;j++){
-//             std::cout<<tiges[j][i];
-//             std::cout<< " ";
-//         }
-//         std::cout<< "\n";
-//     }
-// }
-
-void Etat::add_cube(int num_tige, int cube){
-    if(tiges[num_tige].size() >= 3){
-        std::cout<< "tiges deja pleine\n";
-        exit(0);
+void Etat::print() {
+  std::stack<int> tempStack;
+  int tmp;
+  for (int i = 0; i < 4; i++) {
+    tempStack = tiges[i];
+    cout << "tige " << i << "= ";
+    while (!tempStack.empty()) {
+      std::cout << tempStack.top() << " ";
+      tempStack.pop();
     }
-    tiges[num_tige].push(cube);
+
+    std::cout << "\n";
+  }
 }
 
-int Etat::suppr_cube(int num_tige){
-    if(tiges[num_tige].size() <= 0){
-        std::cout<< "pas de cube a deplacer\n";
-        exit(0);
-    }
-    int cube = tiges[num_tige].top();
-    tiges[num_tige].pop();
-    return cube;
+void Etat::add_cube(int num_tige, int cube) {
+  if (tiges[num_tige].size() >= 3) {
+    std::cout << "tiges deja pleine\n";
+    exit(0);
+  }
+  tiges[num_tige].push(cube);
 }
 
-void Etat::move(int tige_depart, int tige_destination){
-    add_cube(tige_destination, suppr_cube(tige_depart));
+int Etat::suppr_cube(int num_tige) {
+  if (tiges[num_tige].size() <= 0) {
+    std::cout << "pas de cube a deplacer\n";
+    exit(0);
+  }
+  int cube = tiges[num_tige].top();
+  tiges[num_tige].pop();
+  return cube;
+}
+
+void Etat::move(int tige_depart, int tige_destination) {
+  add_cube(tige_destination, suppr_cube(tige_depart));
 }
 
 Etat* Etat::clone(){
@@ -69,19 +73,21 @@ Etat* Etat::clone(){
     return copy;
 }
 
-int main(){
-    Etat *e = new Etat(0);
-    
-    // e->print();
-    std::cout<<"\n";
-    e->add_cube(1, 3);
-    // e->print();
-    std::cout<<"\n";
+int main() {
+  Etat *e = new Etat(0);
 
-    // e->suppr_cube(1);
-    // e->print();
-    // std::cout<<"\n";
-    e->move(1, 2);
-    // e->print();
+  e->print();
+  std::cout << "\n";
+  e->add_cube(1, 3);
+  e->print();
+  std::cout << "\n";
 
+  e->suppr_cube(1);
+  e->print();
+  std::cout << "\n";
+
+  e->add_cube(1, 3);
+
+  e->move(1, 2);
+  e->print();
 }
