8f50aa3227044c58ed180adb5b1f44be1693f8de
Corentin <corentin.ludwig@outlook.fr>
2024-05-10 15:54:57 +0200

    ajoue cout et getteur

--- jeu/plateau.h
+++ jeu/plateau.h
@@ -6,10 +6,14 @@
 
 using namespace std;
 class Etat {
-    public:
+    private:
         std::stack<int> tiges[4];
         int level;
+        int cout;
+    public:
         Etat(int level);
+        int getlevel(){ return level;}
+        int getcout(){ return cout;}
         void print();
         void add_cube(int num_tige, int cube);
         int suppr_cube(int num_tige);
