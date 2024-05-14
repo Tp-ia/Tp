5434c90224e31f1e4b88429f92804cbe25d7294e
Corentin <corentin.ludwig@outlook.fr>
2024-05-14 19:59:05 +0200

    cleanning

--- parcour/ida.cpp
+++ parcour/ida.cpp
@@ -7,10 +7,6 @@
 
 using namespace std;
 
-
-
-
-
 bool contains(Etat *actuel,list<Etat> chemin){//parcours dune liste
     auto it = find(chemin.begin(),chemin.end(),*actuel);
     if(it !=chemin.end()){
@@ -18,59 +14,6 @@ bool contains(Etat *actuel,list<Etat> chemin){//parcours dune liste
     }
     else return false;
 }
-
-
-// retour search(list<Etat> &chemin, int lim,int (*h)(Etat*,Etat*),Etat *but){
-//     retour resultat;
-//     resultat.but=false;
-    
-//     Etat node = chemin.back();
-//     resultat.e = node.clone();
-//     int f = node.getlevel() + node.getcost();
-//     // cout<<"debut if\n";
-//     if(f>lim){
-//         resultat.lim=f;
-//         // cout<<"return premier\n";
-//         resultat.e = node.clone();
-//         return resultat;
-//     }
-//     // cout<<"2eme if\n";
-//     if(node == *but){
-//         resultat.e = node.clone();
-//         resultat.but=true;
-//         return resultat;
-//     }
-//     // cout<<"fin if\n";
-//     int min=MIN;
-//     list<Etat> successors = node.filsEtatIDA(but,h);
-//     for(Etat actuel = successors.front();!successors.empty();){
-//         // cout<<"nouvelle iteration\n";
-//         actuel=successors.front();
-//         if(!contains(&actuel,chemin)){
-//             chemin.push_back(actuel);
-//             // cout<<"appele search interne\n";
-//             resultat = search(chemin,lim,h,but);
-//             // cout<<"fin search interne\n";
-//             if(resultat.but){
-//                 return resultat;
-//             }
-    
-//             if(resultat.lim<min){
-//                 min=resultat.lim;
-//             }
-//             chemin.pop_back();
-//         }
-//         // cout<<"pop succ\n";
-//         successors.pop_front();
-//         // cout<<"reboucle for\n";
-//         // cout<<successors.size()<<" size succ\n";
-//         // cout<<successors.empty()<<" is emppty succ\n";
-//     }
-//     // cout<<"end for\n";
-//     return resultat;
-// }
-
-// //RETOUR Etat OU PATH?
 // retour ida_star(Etat *initial,Etat *but,int (*h)(Etat*,Etat*)){
 //     int compteur = 0;
 //     int lim = h(initial,but);
@@ -145,7 +88,6 @@ retour ida_star(Etat *initial,Etat *but,int (*h)(Etat*,Etat*)){
 
     while(!r.but){
         compteur++;
-        cout<< r.lim <<"\n";
         r = ProfondeurDAbordBorneeIDA(initial, but,r.lim,h);
     }
     // r.l = recreateGenealogie(r.e);
